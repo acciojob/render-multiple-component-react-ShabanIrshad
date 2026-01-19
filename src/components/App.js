d4d088976f1c3c1ea87bde16c5ce7d5f834f6c85
Shaban Irshad <shabanirshad408@gmail.com>
2026-01-19 13:00:27 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,22 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className='ns-wrapper'>
+        <h1 data-ns-test='project-name'>Transaction Blog</h1>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+         <h2 data-ns-test='project-name'>Transaction Blog</h2>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+         <h3 data-ns-test='project-name'>Transaction Blog</h3>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+         <h4 data-ns-test='project-name'>Transaction Blog</h4>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+         <h5 data-ns-test='project-name'>Transaction Blog</h5>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+         <h6 data-ns-test='project-name'>Transaction Blog</h6>
+        <p data-ns-test='project-description'>This is a project about the transactional base informations to exchange the news about growing business and development based.</p>
+      </div>
+    </div>
   )
 }
 
